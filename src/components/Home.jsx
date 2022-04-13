5b41ad150929a76fa282c857cf8f92c38393f0ff
Alimul Hassan <alhassan069@gmail.com>
2022-04-14 03:26:32 +0530

    made deployable  build files

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,7 @@ import { data } from "../data/data";
 import { useState } from "react";
 
 export const Home = () => {
-  const [doctorData, setDoctorData] = useState(data);
+  const [doctorData] = useState(data);
   return (
     <>
       <h1>Book Appointment</h1>
